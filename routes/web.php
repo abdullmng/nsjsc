353b5d17df0797e044f07d09fef7522d864ed4fd
Muhammad Abdullahi <abdullmng@gmail.com>
2024-01-04 22:01:11 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [AuthController::class, 'login'])->name('login');
+Route::get("/", function () {
+    return redirect(route('login'));
+});
+Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class,'authenticate'])->name('authenticate');
 Route::get('/forgot', [AuthController::class,'forgot'])->name('forgot');
 Route::post('/forgot', [AuthController::class,'requestReset'])->name('password.request');
