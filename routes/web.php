909c0c32bf56e3b7c013fd1021987a4208a413d6
Muhammad Abdullahi <abdullmng@gmail.com>
2024-08-26 00:23:01 +0100

    add home page

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/", function () {
-    return redirect(route('login'));
-});
+    return view('welcome');
+})->name('home');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class,'authenticate'])->name('authenticate');
 Route::get('/forgot', [AuthController::class,'forgot'])->name('forgot');
